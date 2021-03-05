df3698d45c1a71480f16b82aad6bd9d10deda80b
liuqiqi <liu677688@163.com>
2021-03-05 22:51:19 +0800

    搭建基础框架

---
+++ record-content/src/test/java/com/liuqiqi/record/content/ContentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liuqiqi.record.content;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ record-question/src/test/java/com/liuqiqi/record/question/QuestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liuqiqi.record.question;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
